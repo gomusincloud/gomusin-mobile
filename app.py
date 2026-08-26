e82a218c28d5820c88e89ba4fecfbe95559bcc42
gomusincloud <l___86@naver.com>
2026-08-26 13:03:17 +0900

    Update app.py

--- app.py
+++ app.py
@@ -176,6 +176,8 @@ def init_data():
         db.session.commit()
 
 if __name__ == "__main__":
-    with app.app_context():
-        init_data()
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT",5000)), debug=True)
+   with app.app_context():
+    db.create_all()
+    sync_admin()with app.app_context():
+    db.create_all()
+    sync_admin()
